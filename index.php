af104f7c7dc8ec018af80cb33bb958a36ecf3a42
wkasel <william@jumpfox.com>
2013-09-06 00:42:27 -0700

    Updated Readme

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+  // =================================================================================
+  // = This is only so we can easily push to heroku, you can scratch this if need-be =
+  // =================================================================================
   echo file_get_contents("index.html")
 ?>
